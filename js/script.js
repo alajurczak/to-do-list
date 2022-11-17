5876b4f177a12a60cd368fd52edbaa763ed5aa55
Alicja Jurczak <ala.jurczak@gmail.com>
2022-11-17 13:35:30 +0100

    example tasks added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,50 @@
   const welcome = () => {
     console.log("Hello!");
   }
-welcome();
+  welcome();
+
+
+  const tasks = [
+    {
+      content: "obejrzeć lekcję",
+      done: true,
+    },
+    {
+      content: "wykonać zadanie domowe",
+      done: false,
+    },
+  ];
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+      <li>
+        ${task.content}
+      </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
+
+
+
+
+
+
+
+
+
+
+
+  const init = () => {
+    render();
+
+
+  };
+
+  init();
 
 }
